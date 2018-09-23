b06f25ab8aaaaf4a4ccffd3501931545d7970526
kuanweih <kuanweih@andrew.cmu.edu>
2018-09-22 20:45:57 -0400

    encoded condition from Koposov et al 2017

--- get_ra_dec.py
+++ get_ra_dec.py
@@ -9,11 +9,22 @@ FILENAME = 'stars-coord'    # output file name
 INFOFILE = 'stars-coord-attr'    # info of the map
 
 
-# TODO: to be moved to paramfile
-G_MAG_CUT = 0  # condition for g band mag cut
-G_MAG_MIN = 17
-G_MAG_MAX = 21
-G_MAG_ASTRO_EX_NOISE_CUT = 1    # condition for g_mag dependent cut on noise
+# HOST, USER, PASSWORD from personal txt file
+with open(PERSONAL_TXT, 'r') as info_file:
+    for line in info_file:
+        exec(line)
+
+
+# RA, DEC, RADIUS, DATABASE, CATALOGS etc... from parameter file
+with open(PARAMETER_TXT, 'r') as param_file:
+    for line in param_file:
+        exec(line)
+
+
+if ASTRO_EX_NOISE_G_MAG_CUT == 1:
+    """ simplify eq 1 in Koposov et al 2017 (MNRAS 470) into exponential form """
+    def con_arr_astro_ex_noise_g_mag(astro_ex_noise, g_mag):
+        return astro_ex_noise - 10.**(0.15 * (g_mag - 15.) + 0.25)
 
 
 def cut_datas(datas, con_arr, min_val, max_val):
@@ -30,18 +41,6 @@ def cut_datas(datas, con_arr, min_val, max_val):
     return [data[mask] for data in datas]
 
 
-# HOST, USER, PASSWORD from personal txt file
-with open(PERSONAL_TXT, 'r') as info_file:
-    for line in info_file:
-        exec(line)
-
-
-# RA, DEC, RADIUS, DATABASE, CATALOGS from parameter file
-with open(PARAMETER_TXT, 'r') as param_file:
-    for line in param_file:
-        exec(line)
-
-
 # query data from DATABASE
 query_str = 'select {} from {} where q3c_radial_query(ra, dec, {}, {}, {})'.format(
             CATALOGS, DATABASE, RA, DEC, RADIUS)
@@ -51,10 +50,15 @@ datas = sqlutilpy.get(query_str, host=HOST, user=USER, password=PASSWORD)
 if G_MAG_CUT == 1:
     datas = cut_datas(datas, datas[2], G_MAG_MIN, G_MAG_MAX)
 
+if ASTRO_EX_NOISE_G_MAG_CUT == 1:
+    datas = cut_datas(datas, con_arr_astro_ex_noise_g_mag(datas[3], datas[2]),
+                      ASTRO_EX_NOISE_G_MAG_MIN, ASTRO_EX_NOISE_G_MAG_MAX)
+
 
 # TODO change index to correspond with the CATALOGS string
 ra = datas[0]
 dec = datas[1]
+
 
 # output
 np.save(FILENAME, np.array([ra, dec]))
