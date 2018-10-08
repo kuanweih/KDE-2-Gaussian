e68329cf955573e9c610c4c87854b0afaa1aeb44
Kw Huang <kuanweih@andrew.cmu.edu>
2018-10-08 15:59:57 -0400

    parametrize the ratio of taget and bg area

--- density-estimation.py
+++ density-estimation.py
@@ -26,7 +26,7 @@ def gaussian(x, y, s):
 def od_gaussian(x, y, star_x, star_y, s):
     """
     overdensity on a mesh with a 2d Gaussian filter
-    x, y: mesh arrays, star_x, star_y: position of stars
+    x, y: mesh arrays. star_x, star_y: position of stars
     """
     od = np.sum(np.array([gaussian(x - star_x[i], y - star_y[i], s)
                           for i in range(len(star_x))]), axis=0)
@@ -41,19 +41,19 @@ def distance2(x_arr, y_arr, x_cen, y_cen):
     return d2
 
 
-def od_poisson(x, y, star_x, star_y, s1, r, s2):
+def od_poisson(x, y, star_x, star_y, s1, s2, r12):
     """
     overdensity on a mesh with a Poisson CDF
-    x, y: mesh arrays, star_x, star_y: position of stars
-    s1, r, s2: inner, middle, and outer scales
+    x, y: mesh arrays. star_x, star_y: position of stars
+    s1, s2: inner and outer scales
+    r12: ratio of area between target and background
     """
     from scipy.stats import poisson
+    r = np.sqrt(s2**2 - r12 * s1**2)    # middle radius
+
     n_inner = np.sum(np.array([(distance2(x, y, star_x[i], star_y[i]) < s1**2)
                                for i in range(len(star_x))]), axis=0)
-
-    # TODO: debugging: test middle r value
-    r = np.sqrt(s2**2 - 10. * s1**2)
-
+                               
     n_outer = np.sum(np.array([(r**2 < distance2(x, y, star_x[i], star_y[i])) *
                                (distance2(x, y, star_x[i], star_y[i]) < s2**2)
                                for i in range(len(star_x))]), axis=0)
@@ -68,8 +68,7 @@ def od_poisson(x, y, star_x, star_y, s1, r, s2):
     return od
 
 
-def significance(x, y, s1, s2, star_x, star_y,
-                 kernel_bg='gaussian', poisson_r=0):
+def significance(x, y, s1, s2, star_x, star_y, kernel_bg='gaussian', r12=0):
     """
     get significance on a mesh with a Poisson CDF
     x, y: mesh arrays, star_x, star_y: position of stars
@@ -81,12 +80,13 @@ def significance(x, y, s1, s2, star_x, star_y,
         od_2 = od_gaussian(x, y, star_x, star_y, s2)
     elif kernel_bg == 'poisson':
         od_1 = od_gaussian(x, y, star_x, star_y, s1)
-        od_2 = od_poisson(x, y, star_x, star_y, s1, poisson_r, s2)
+        od_2 = od_poisson(x, y, star_x, star_y, s1, s2, r12)
     else:
         print('wrong kernel :(')
     """
     od_1: overdensity of target region + background
     od_2: background overdensity
+    sigma: sigma for od_1
     """
     sigma = od_gaussian(x, y, star_x, star_y, s2) / (4. * np.pi * s1**2)
     sigma = np.sqrt(sigma)
@@ -122,7 +122,7 @@ xx, yy = np.meshgrid(x, y, sparse=True)  # TODO: what does sparse mean?
 
 # sig = significance(xx, yy, SIGMA1, SIGMA2, coords[0], coords[1])
 sig = significance(xx, yy, SIGMA1, SIGMA2, coords[0], coords[1],
-                   kernel_bg=KERNEL_BG, poisson_r=POISSON_MIDDLE_R)
+                   kernel_bg=KERNEL_BG, poisson_r=RATIO_AREA_TG_BG)
 print(sig)
 np.save(SIGNI_FILE, sig)
 np.save(MESHFILE, np.array([x, y]))
