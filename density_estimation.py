f3d5ab112d816663850b45b88b1a72b5cb0524f8
Kw Huang <kuanweih@andrew.cmu.edu>
2018-11-08 19:00:09 -0500

    z score not done yet

--- density_estimation.py
+++ density_estimation.py
@@ -46,7 +46,12 @@ def sig_poisson(x, y, s1, s2, star_x, star_y, r12):
                                (distance2(x, y, star_x[i], star_y[i]) < r**2)
                                for i in range(len(star_x))]), axis=0)
 
-    sig = stats.zscore(poisson.cdf(n_inner, n_outer / r12))
+    """
+    TODO: Need to find out what scale to use...
+          What we have here may generate inf z score (cdf = 1)...
+    """
+    lambda_poisson = n_outer / r12
+    sig = poisson.ppf(poisson.cdf(n_inner, lambda_poisson), lambda_poisson)
     return sig
 
 
