116bdb43e5475e2e4725364ce9bf94bdbc5b9be5
Kw Huang <kuanweih@andrew.cmu.edu>
2019-02-21 14:42:48 -0500

    cleaned some useless comments

--- classKDE_MWSatellite.py
+++ classKDE_MWSatellite.py
@@ -87,52 +87,7 @@ class KDE_MWSatellite(MWSatellite):
 
 
 
-
-
-# def sig_2_gaussian(x, y, s1, s2, star_x, star_y):
-#     """
-#     get significance using 2 Gaussian kernels.
-#     x, y: mesh arrays
-#     star_x, star_y: position of stars
-#     s1, s2: target and background scales
-#     """
-#     from scipy.ndimage import gaussian_filter
-#     hist2d, x, y = np.histogram2d(star_y, star_x, bins=(y, x))
-#     od_1 = gaussian_filter(hist2d, s1)
-#     od_2 = gaussian_filter(hist2d, s2)
-#     sig = (od_1 - od_2) / np.sqrt(od_2 / (4. * np.pi * s1**2))
-#     return sig
-
-
-
-# def sig_2_gaussian(x, y, s1, s2, star_x, star_y):
-#     """
-#     get significance using 2 Gaussian kernels.
-#     x, y: mesh arrays
-#     star_x, star_y: position of stars
-#     s1, s2: target and background scales
-#     """
-#     from scipy.ndimage import gaussian_filter
-#     hist2d, x, y = np.histogram2d(star_y, star_x, bins=(y, x))
-#     od_1 = gaussian_filter(hist2d, s1)
-#     od_2 = gaussian_filter(hist2d, s2)
-#     sig = (od_1 - od_2) / np.sqrt(od_2 / (4. * np.pi * s1**2))
-#     return sig
-#
-# def get_grid_coord(center, width_mesh, num_grid):
-#     """
-#     get grid coordinates according to the center position and width of the mesh
-#     """
-#     coord = np.linspace(center - 0.5 * width_mesh,
-#                         center + 0.5 * width_mesh, num=num_grid, endpoint=True)
-#     return coord
-
-
-
-
-
-
-
+#TODO: add poisson statistics method into the class
 
 # def distance2(x_arr, y_arr, x_cen, y_cen):
 #     """
@@ -160,28 +115,4 @@ class KDE_MWSatellite(MWSatellite):
 #     lambda_poisson = n_outer * r12    # estimated background count
 #     sig = (n_inner - lambda_poisson) / np.sqrt(lambda_poisson)    # z score
 #     return sig
-#
-#
-# def sig_2_gaussian(x, y, s1, s2, star_x, star_y):
-#     """
-#     get significance using 2 Gaussian kernels.
-#     x, y: mesh arrays
-#     star_x, star_y: position of stars
-#     s1, s2: target and background scales
-#     """
-#     from scipy.ndimage import gaussian_filter
-#     hist2d, x, y = np.histogram2d(star_y, star_x, bins=(y, x))
-#     od_1 = gaussian_filter(hist2d, s1)
-#     od_2 = gaussian_filter(hist2d, s2)
-#     sig = (od_1 - od_2) / np.sqrt(od_2 / (4. * np.pi * s1**2))
-#     return sig
-#
-#
-# def get_grid_coord(center, width_mesh, num_grid):
-#     """
-#     get grid coordinates according to the center position and width of the mesh
-#     """
-#     coord = np.linspace(center - 0.5 * width_mesh,
-#                         center + 0.5 * width_mesh, num=num_grid, endpoint=True)
-#     return coord
 #
