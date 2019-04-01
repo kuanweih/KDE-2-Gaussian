a7ffcb69bab9df886f17d9f66b307cf3b40d63d7
kuanweih <kuanweih@andrew.cmu.edu>
2019-03-31 20:09:42 -0400

    added slurm job script

---
+++ slurm-coma.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --export=NONE
+#SBATCH --partition=long
+#SBATCH --ntasks=20
+#SBATCH --time=72:00:00
+
+source  activate  mypython3    # use py3 on coma (don't need this if already using py3)
+
+python  main.py  --name_dwarf  "Fornax"
+python  main.py  --name_dwarf  "Fornax"  --scale_sigma2  2.0
