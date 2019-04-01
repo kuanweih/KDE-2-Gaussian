450ac3267ccbd3e337535625557e208f97839f65
kuanweih <kuanweih@andrew.cmu.edu>
2019-03-31 20:44:32 -0400

    more detail in slurm

--- slurm-coma.sh
+++ slurm-coma.sh
@@ -6,5 +6,13 @@
 
 source  activate  mypython3    # use py3 on coma (don't need this if already using py3)
 
-python  main.py  --name_dwarf  "Fornax"
-python  main.py  --name_dwarf  "Fornax"  --gc_size_pc  20  --scale_sigma2  2.0
+name_dwarf="Fornax"
+gc_size_pcs='10 20 30 40 50'
+
+# python  main.py  --name_dwarf  $name_dwarf
+
+
+for gc_size in $gc_size_pcs;  do
+
+python  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size
+python  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size  --scale_sigma2  2.0
