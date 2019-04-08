78bcfd36f4a9ccbfe68fb1abb53edca47009493c
Kw Huang <kuanweih@andrew.cmu.edu>
2019-04-08 16:31:14 -0400

    read txt for dwarfs

--- submit-slurm.sh
+++ submit-slurm.sh
@@ -7,21 +7,20 @@
 source  activate  mypython3    # use py3 on coma (don't need this if already using py3)
 
 
-name_dwarfs="Fornax  UrsaMajorII  UrsaMinor"
-# name_dwarfs="LeoIV  LeoV  LeoII"
-# name_dwarfs="LeoI  Eridanus2  Hercules"
-# name_dwarfs="Carina  Sculptor  *Eridanus3"
+# name_dwarfs="Fornax  UrsaMajorII  UrsaMinor"
+# name_dwarfs="*Eridanus3  Eridanus2  Hercules"
 
 
-gc_size_pcs='1  5  10'
+gc_size_pcs="10"
+# gc_size_pcs="5  10"
 
-# python  main.py  --name_dwarf  $name_dwarf
+input="dwarfs-McConnachie/dwarfs-names.txt"
 
-for name_dwarf in $name_dwarfs;  do
-for gc_size in $gc_size_pcs;  do
-
-python  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size
-python  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size  --scale_sigma2  2.0
-
-done
-done
+while IFS= read -r name_dwarf;  do
+  for gc_size in $gc_size_pcs;  do
+    # echo $name_dwarf
+    # echo $gc_size
+    python  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size
+    # python  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size  --scale_sigma2  2.0
+  done  # for
+done < "$input"  # while
