c90a624d4eb1ef36ec3039e4394d8aeacf1ed2f1
Kw Huang <kuanweih@andrew.cmu.edu>
2019-04-26 13:56:16 -0400

    added summary

--- summarize.py
+++ summarize.py
@@ -4,6 +4,17 @@ import glob
 import pandas as pd
 
 
+def create_dir(dir_name: str):
+    """ create directory with a name 'dir_name' """
+    import os
+    import errno
+    if not os.path.exists(dir_name):
+        try:
+            os.makedirs(dir_name)
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
 # MODE = "create_csv"
 MODE = "read_csv"
 
@@ -32,10 +43,10 @@ if __name__ == '__main__':
             sig_p_pm = np.load("{}/sig_poisson-pm_error5.npy".format(path))
 
             sig_g_peak = np.sum(sig_g > s_above)
-            sig_g_pm_peak = np.sum(sig_g > s_above)
+            sig_g_pm_peak = np.sum(sig_g_pm > s_above)
 
             sig_p_peak = np.sum(sig_p > s_above)
-            sig_p_pm_peak = np.sum(sig_p > s_above)
+            sig_p_pm_peak = np.sum(sig_p_pm > s_above)
 
             names.append(name)
             gc_sizes.append(gc_size)
@@ -61,9 +72,53 @@ if __name__ == '__main__':
 
         df.to_csv("summary.csv", index=False)
 
+
+
     if MODE == "read_csv":
+        # create dir for searching results
+        path = "tables"
+        create_dir(path)
+
         df = pd.read_csv("summary.csv")
-        print(df)
+
+        pixel_th = 12
+        n_star_th = 300
+        gc_sizes = [6, 7, 8, 9, 10]
+
+        target_dwarfs = []
+
+        for gc_size in gc_sizes:
+            mask = df["sig_p_peak"] > pixel_th
+            mask = (df["sig_p_pm_peak"] > pixel_th) | mask
+
+            mask = (df["n_star"] > n_star_th) & mask
+            mask = (df["n_star_pm"] > n_star_th) & mask
+
+            mask = (df["gc_size"] == gc_size) & mask
+
+            df[mask].to_csv("{}/gc{}.csv".format(path, gc_size), index=False)
+
+            for name in df[mask]["name"]:
+                if name not in target_dwarfs:
+                    target_dwarfs.append(name)
+
+        for target_dwarf in target_dwarfs:
+            print(target_dwarf)
+
+        for target_dwarf in target_dwarfs:
+            mask = df["sig_p_peak"] > pixel_th
+            mask = (df["sig_p_pm_peak"] > pixel_th) | mask
+
+            mask = (df["n_star"] > n_star_th) & mask
+            mask = (df["n_star_pm"] > n_star_th) & mask
+
+            mask = (df["name"] == target_dwarf) & mask
+
+            df[mask].to_csv("{}/{}.csv".format(path, target_dwarf), index=False)
+
+
+
+        # print(df[mask])
 
 
 
