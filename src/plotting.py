5ce8106d2734081a47998ca1840a9dc63a56d583
kuanweih <kuanweih@andrew.cmu.edu>
2019-05-15 12:46:43 -0400

    enlarged data points for the stars

--- src/plotting.py
+++ src/plotting.py
@@ -7,9 +7,16 @@ from param.param import *
 import scipy.stats as stats
 
 
-def visualize_4_panel(path: str, outfile: str, n_error: float, kernel: str, s_above=5):
-    """
-    kernel: 'gaussian' or 'poisson'
+def visualize_4_panel(path: str, outfile: str, n_error: float, kernel: str,
+                      s_above=5):
+    """ Plotting star distribution (left panels) and density maps (right
+    panels). Top row: all stars. Bottom row: pm selection.
+
+    : path : path of all results files
+    : outfile : result file of the dwarf
+    : n_error : N_ERRORBAR for pm selection
+    : kernel : 'gaussian' or 'poisson'
+    : s_above : significance threshold, default value = 5
     """
     sns.set(style="white", color_codes=True, font_scale=1)
     fig, axes = plt.subplots(2, 2, figsize=(10, 10))
@@ -23,35 +30,49 @@ def visualize_4_panel(path: str, outfile: str, n_error: float, kernel: str, s_ab
             np.load('{}/sig_{}-pm_error{}.npy'.format(path, kernel, n_error))]
 
     datas = [np.load('{}/queried-data.npy'.format(path)).item(),
-             np.load('{}/queried-data-pm_error{}.npy'.format(path, n_error)).item()]
+             np.load('{}/queried-data-pm_error{}.npy'.format(path,
+                                                             n_error)).item()]
 
     ras = [data["ra"] for data in datas]
     decs = [data["dec"] for data in datas]
 
-    masks = [data["sig_{}".format(kernel)] > s_above for data in datas]    # masks for stars with sig > s_above
+    # masks for stars with sig > s_above
+    masks = [data["sig_{}".format(kernel)] > s_above for data in datas]
 
     n_stars = [len(data["ra"]) for data in datas]
 
     extent = [x.min(), x.max(), y.min(), y.max()]    # arg extent for imshow
 
     for v in range(2):
-        axes[v, 0].imshow(sigs[v] > s_above, cmap='copper', vmin=-0.01, vmax=1.01, extent=extent, origin='lower')
-        axes[v, 0].plot(ras[v], decs[v], '.', c='deepskyblue', markersize=0.5, alpha=0.5)
-        axes[v, 0].plot(ras[v][masks[v]], decs[v][masks[v]], '.', c='orange', markersize=0.5)
+        axes[v, 0].imshow(sigs[v] > s_above, cmap='copper',
+                          vmin=-0.01, vmax=1.01, extent=extent, origin='lower')
+        axes[v, 0].plot(ras[v], decs[v],
+                        '.', c='deepskyblue', markersize=0.5, alpha=0.5)
+        axes[v, 0].plot(ras[v][masks[v]], decs[v][masks[v]],
+                        '.', c='orange', markersize=1)
         axes[v, 0].set_title('all: {} stars'.format(n_stars[v]) if v==0 else 'pm: {} stars'.format(n_stars[v]))
 
-        axes[v, 1].imshow(sigs[v] > s_above, cmap='copper', vmin=-0.01, vmax=1.01, extent=extent, origin='lower')
+        axes[v, 1].imshow(sigs[v] > s_above, cmap='copper',
+                          vmin=-0.01, vmax=1.01, extent=extent, origin='lower')
         axes[v, 1].set_title('sig > {}: {} pixels'.format(s_above, np.sum(sigs[v] > s_above)))
 
         for u in range(2):
-            axes[v, u].tick_params(axis='both', which='both', labelleft=False, labelbottom=False)
+            axes[v, u].tick_params(axis='both', which='both',
+                                   labelleft=False, labelbottom=False)
 
     plt.savefig("{}-{}.png".format(outfile, kernel), bbox_inches='tight', dpi=300)
 
 
-def hist_2_panel(path: str, outfile: str, n_error: float, kernel: str, s_above=5):
-    """
-    kernel: 'gaussian' or 'poisson'
+def hist_2_panel(path: str, outfile: str, n_error: float, kernel: str,
+                 s_above=5):
+    """ Plotting histograms (left panels) and normalized histograms (right
+    panels). Top row: all stars. Bottom row: pm selection.
+
+    : path : path of all results files
+    : outfile : result file of the dwarf
+    : n_error : N_ERRORBAR for pm selection
+    : kernel : 'gaussian' or 'poisson'
+    : s_above : significance threshold, default value = 5
     """
     sns.set(style="white", color_codes=True, font_scale=1)
     fig, axes = plt.subplots(2, 2, figsize=(10, 10))
