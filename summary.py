c4bb57a74970c7dcff0ec1fb120da12b44d70773
kuanweih <kuanweih@andrew.cmu.edu>
2019-05-22 10:33:39 -0400

    draft of plotting hips images

--- summary.py
+++ summary.py
@@ -3,7 +3,20 @@ import numpy as np
 import glob
 import pandas as pd
 
+from multiprocessing import Process
+
 from src.tools import create_dir, df_concat
+from src.hips_image import plot_hips_sky_image
+
+
+
+def mask_pixel_star_th(df: pd.DataFrame, pixel_th: int, n_star_th:int) -> np.ndarray:
+    mask = df["sig_p_peak"] > pixel_th
+    mask = (df["sig_p_pm_peak"] > pixel_th) | mask
+    mask = (df["n_star"] > n_star_th) & mask
+    mask = (df["n_star_pm"] > n_star_th) & mask
+    return  mask
+
 
 
 if __name__ == '__main__':
@@ -36,13 +49,11 @@ if __name__ == '__main__':
 
         sig_g = np.load("{}/sig_gaussian.npy".format(path))
         sig_g_pm = np.load("{}/sig_gaussian-pm_error5.npy".format(path))
-
         sig_p = np.load("{}/sig_poisson.npy".format(path))
         sig_p_pm = np.load("{}/sig_poisson-pm_error5.npy".format(path))
 
         sig_g_peak = np.sum(sig_g > s_above)
         sig_g_pm_peak = np.sum(sig_g_pm > s_above)
-
         sig_p_peak = np.sum(sig_p > s_above)
         sig_p_pm_peak = np.sum(sig_p_pm > s_above)
 
@@ -66,7 +77,8 @@ if __name__ == '__main__':
     searching_table["sig_p_pm_peak"] = sig_p_pm_peaks
 
     df = pd.DataFrame(data=searching_table)
-    df = df[["name", "gc_size", "n_star", "n_star_pm", "sig_g_peak", "sig_g_pm_peak", "sig_p_peak", "sig_p_pm_peak"]]
+    df = df[["name", "gc_size", "n_star", "n_star_pm",
+             "sig_g_peak", "sig_g_pm_peak", "sig_p_peak", "sig_p_pm_peak"]]
 
     df.to_csv("{}/summary.csv".format(output_file), index=False)
 
@@ -82,12 +94,7 @@ if __name__ == '__main__':
     target_dwarfs = []
 
     for gc_size in gc_sizes:
-        mask = df["sig_p_peak"] > pixel_th
-        mask = (df["sig_p_pm_peak"] > pixel_th) | mask
-
-        mask = (df["n_star"] > n_star_th) & mask
-        mask = (df["n_star_pm"] > n_star_th) & mask
-
+        mask = mask_pixel_star_th(df, pixel_th, n_star_th)
         mask = (df["gc_size"] == gc_size) & mask
 
         df[mask].to_csv("{}/gc{}.csv".format(path, gc_size), index=False)
@@ -97,12 +104,68 @@ if __name__ == '__main__':
                 target_dwarfs.append(name)
 
     for target_dwarf in target_dwarfs:
-        mask = df["sig_p_peak"] > pixel_th
-        mask = (df["sig_p_pm_peak"] > pixel_th) | mask
-
-        mask = (df["n_star"] > n_star_th) & mask
-        mask = (df["n_star_pm"] > n_star_th) & mask
-
+        mask = mask_pixel_star_th(df, pixel_th, n_star_th)
         mask = (df["name"] == target_dwarf) & mask
 
         df[mask].to_csv("{}/{}.csv".format(path, target_dwarf), index=False)
+
+
+    """ Create images via hips """
+    # hips_surveys = ['IPAC/P/GLIMPSE360']
+    hips_surveys = ['CDS/P/DSS2/red']
+    path = "images"
+    create_dir(path)
+
+    df = pd.read_csv('summary/all_pixels_peaks.csv')
+
+    name_set = set(df['name'])
+    for name in name_set:
+        df_name = df.loc[df['name'] == name]
+        label_set = set(df_name['label'])
+        for label in label_set:
+            df_name_label = df_name.loc[df_name['label'] == label]
+            ra = np.mean(df_name_label['ra'])
+            dec = np.mean(df_name_label['dec'])
+            name_list = name.split("-")
+            width = 5. * float(name_list[5].split('s')[1])
+
+            for hips_survey in hips_surveys:
+                plot_hips_sky_image(ra, dec, width, hips_survey, path, name, label, 1000)
+
+
+    # # instantiating process with arguments
+    # for name in names:
+    #     proc = Process(target=print_func, args=(name,))
+    #     procs.append(proc)
+    #     proc.start()
+    #
+    # # complete the processes
+    # for proc in procs:
+    #     proc.join()
+
+
+
+
+
+
+
+# def print_func(continent='Asia'):
+#     print('The name of continent is : ', continent)
+#
+# if __name__ == "__main__":  # confirms that the code is under main function
+#     names = ['America', 'Europe', 'Africa']
+#     procs = []
+#     proc = Process(target=print_func)  # instantiating without any argument
+#     procs.append(proc)
+#     proc.start()
+
+    # # instantiating process with arguments
+    # for name in names:
+    #     # print(name)
+    #     proc = Process(target=print_func, args=(name,))
+    #     procs.append(proc)
+    #     proc.start()
+    #
+    # # complete the processes
+    # for proc in procs:
+    #     proc.join()
