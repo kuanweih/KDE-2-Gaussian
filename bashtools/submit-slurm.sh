4c81698d14d07dbed8b326f7afdf6302ca1c30ef
kuanweih <kuanweih@andrew.cmu.edu>
2019-06-09 10:13:12 -0400

    added splitted list

--- bashtools/submit-slurm.sh
+++ bashtools/submit-slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --export=NONE
 #SBATCH --partition=long
+#SBATCH --constraint=intel_e5_v4
 #SBATCH --ntasks-per-node=20
 #SBATCH --time=72:00:00
 
@@ -12,8 +13,9 @@ source  activate  mypython3
 gc_size_pcs="10"
 # gc_size_pcs="5  10"
 
+# input="dwarfs/dwarfs-names.txt"
+input="dwarfs/dwarfs-names-split.txt"
 
-input="dwarfs/dwarfs-names.txt"
 while IFS= read -r name_dwarf;  do
   for gc_size in $gc_size_pcs;  do
     python  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size
