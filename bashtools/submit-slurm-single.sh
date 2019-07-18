00cc6d67f21bbea459fc1d19915b431cf1bdc541
Kw Huang <kuanweih@andrew.cmu.edu>
2019-07-18 22:41:13 +0800

    parallel submit

---
+++ bashtools/submit-slurm-single.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH  --export=NONE
+#SBATCH  --partition=long
+#SBATCH  --constraint=intel_e5_v4
+#SBATCH  --ntasks-per-node=20
+#SBATCH  --time=24:00:00
+#SBATCH  --job-name=dwarf
+
+
+# use py3 on coma (don't need this if already using py3)
+source  activate  mypython3
+
+name_dwarf="$1"
+gc_size="$2"
+
+python  -W  ignore  main.py  --name_dwarf  $name_dwarf  --gc_size_pc  $gc_size
