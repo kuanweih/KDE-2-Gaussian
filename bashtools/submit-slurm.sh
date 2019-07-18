d847f4142f26ad4ca36a8fefd1b15721cd9bb2c4
Kw Huang <kuanweih@andrew.cmu.edu>
2019-07-18 22:56:21 +0800

    make sbatch logs in bashtools

--- bashtools/submit-slurm.sh
+++ bashtools/submit-slurm.sh
@@ -12,7 +12,7 @@ cd  ../McConnachie
 python  convert_McConnachie.py
 cd  ..
 python  joint_list.py
-cd ..
+cd  ../bashtools
 
 
 # main search
@@ -20,12 +20,12 @@ cd ..
 gc_size_pcs="5  10"
 
 # input="dwarfs/dwarfs-names.txt"
-input="dwarfs/dwarfs-names-split.txt"
+input="../dwarfs/dwarfs-names-split.txt"
 
 
 while IFS= read -r name_dwarf;  do
   for gc_size in $gc_size_pcs;  do
-    sbatch  bashtools/submit-slurm-single.sh  $name_dwarf  $gc_size
+    sbatch  submit-slurm-single.sh  $name_dwarf  $gc_size
   done  # for
 done < "$input"  # while
 
