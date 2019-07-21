9ac09ff6b68b3ddc67580674a6fe78a3e788b0b7
Kw Huang <kuanweih@andrew.cmu.edu>
2019-07-21 20:00:26 +0800

    added pm cut

--- src/classMWSatellite.py
+++ src/classMWSatellite.py
@@ -2,10 +2,11 @@ import numpy as np
 import sqlutilpy
 
 
-class MWSatellite(object):
+
+class PatchMWSatellite(object):
     def __init__(self, name_sat: str, ra_sat: float, dec_sat: float,
                  dist: float, width: float, database: str, catalog_str: str):
-        """ Milky Way (MW) Satellite object.
+        """ Milky Way (MW) Satellite object within a patch.
 
         : name_sat : name of the satellite, e.g. Fornax
         : ra_sat : RA of the satellite in deg
@@ -22,20 +23,21 @@ class MWSatellite(object):
         self.width = width
         self.database = database
         self.catalog_str = catalog_str
-
-        str_to_list = self.catalog_str.replace("\n", "").replace(" ", "")
-        self.catalog_list = str_to_list.split(",")
-
+        self.catalog_list = catalog_str.replace("\n", "").replace(" ", "").split(",")
         self.datas = {}
 
     def __str__(self):
-        str1 = "This is a MWSatellite object:\n"
+        str1 = "This is a PatchMWSatellite object:\n"
         str2 = "    name = {}\n".format(self.name_sat)
         str3 = "    ra = {}\n    dec = {}\n".format(self.ra_sat, self.dec_sat)
         str4 = "    map width = {} deg\n".format(self.width)
         str5 = "    database = {}\n".format(self.database)
         str = "{}{}{}{}{}".format(str1, str2, str3, str4, str5)
         return str
+
+    def n_source(self) -> int:
+        """ Calculate the number of stars in the patch """
+        return  len(self.datas[self.catalog_list[0]])
 
     def sql_get(self, host: str, user: str, password: str):
         """ Query 'catalog_str' from 'database' using sqlutilpy.get() """
@@ -50,12 +52,14 @@ class MWSatellite(object):
                     """.format(self.catalog_str, self.database,
                                ra_min, ra_max, dec_min, dec_max)
 
+        print("Querying data in the patch using sqlutilpy.get():")
         datas = sqlutilpy.get(query_str,
                               host=host, user=user, password=password)
 
         # update 'datas' dic to store queried data
         for i, catalog in enumerate(self.catalog_list):
             self.datas[catalog] = datas[i]
+        print("    %d sources are queried \n"  %self.n_source())
 
     def cut_datas(self, mask: np.ndarray):
         """ Cut datas based on the mask. """
@@ -64,16 +68,20 @@ class MWSatellite(object):
 
     def mask_cut(self, catalog: str, min_val: float, max_val: float):
         """ Cut the data with a min and a max value """
+        print("Applying a cut: {} < {} < {}:".format(min_val, catalog, max_val))
         maskleft = min_val < self.datas[catalog]
         maskright = self.datas[catalog] < max_val
         mask = maskleft & maskright
         self.cut_datas(mask)
+        print("    %d sources left \n"  %self.n_source())
 
     def mask_g_mag_astro_noise_cut(self):
         """ Hard code the astrometric_excess_noise and phot_g_mean_mag cut """
+        print("Applying astrometric_excess_noise and phot_g_mean_mag cut.")
         noise = self.datas["astrometric_excess_noise"]
         g_mag = self.datas["phot_g_mean_mag"]
         maskleft = (g_mag <= 18.) & (noise < np.exp(1.5))
         maskright = (18. < g_mag) & (noise < np.exp(1.5 + 0.3 * (g_mag - 18.)))
         mask = maskleft | maskright
         self.cut_datas(mask)
+        print("    %d sources left \n"  %self.n_source())
