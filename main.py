e2fb9c47a8aada3231851d0bd2caa4174ef3804b
kuanweih <kuanweih@andrew.cmu.edu>
2019-08-05 10:43:00 -0400

    separated pm selection as other kde search instead of combining together

--- main.py
+++ main.py
@@ -66,6 +66,37 @@ def execute_kde_routine(patch: PatchMWSatellite, kdepatch: KDE_MWSatellite):
                              kdepatch.sig_gaussian, kdepatch.sig_poisson)
 
 
+def apply_pm_cut_gaia(patch: PatchMWSatellite, n_error: float):
+    """ Create a patch object for the dwarf so as to get pm limits.
+    Then use the limits to cut the data.
+
+    : patch : patch object
+    : n_error : N_ERRORBAR
+    """
+    _str1 = 'Creating a Dwarf object within'
+    _str2 = 'for proper motion selection: \n'
+    print('{} rh = %0.4f deg {}'.format(_str1, _str2) % R_HALFLIGHT)
+    # hard coding of the size for pm selection.
+    Dwarf = PatchMWSatellite(NAME, RA_DWARF, DEC_DWARF,
+                             DISTANCE, R_HALFLIGHT, DATABASE, CATALOG_STR)
+    print(Dwarf.__str__())
+    pmra_mean, pmra_std, pmdec_mean, pmdec_std = calc_pm_dwarf_gaia(Dwarf)
+
+    del Dwarf    # free the memory though it might not be necessary
+    print('Removed the Dwarf object since it is no longer needed. \n')
+    print_sep_line()
+    print('Selecting proper motion within %d sigma: \n' % n_error)
+
+    pmramax = pmra_mean + n_error * pmra_std
+    pmramin = pmra_mean - n_error * pmra_std
+    pmdecmax = pmdec_mean + n_error * pmdec_std
+    pmdecmin = pmdec_mean - n_error * pmdec_std
+
+    patch.mask_cut("pmra", pmramin, pmramax)
+    patch.mask_cut("pmdec", pmdecmin, pmdecmax)
+    print_sep_line()
+
+
 
 if __name__ == '__main__':
     # create result directory
@@ -78,14 +109,19 @@ if __name__ == '__main__':
     print(Patch.__str__())
 
     Patch.sql_get(HOST, USER, PASSWORD)    # query data
+
+    # cuts based on surveys
     if DATABASE == 'gaia_dr2.gaia_source':
         gaia_patch_gmag_cut_astro_noise_cut(Patch)
+    elif DATABASE == 'panstarrs_dr1.stackobjectthin':
+        Patch.mask_panstarrs_stargalaxy_sep()
 
     Patch.append_is_inside(RA_DWARF, DEC_DWARF, R_HALFLIGHT)    # TODO add a factor here
 
-
     print_sep_line()
 
+    if IS_PM_ERROR_CUT:
+        apply_pm_cut_gaia(Patch, N_ERRORBAR)
 
     print('Creating a KDEPatch object and start the KDE calcuation: \n')
     KDEPatch = KDE_MWSatellite(RA, DEC, WIDTH, PIXEL_SIZE, SIGMA1, SIGMA2, SIGMA3)
@@ -97,118 +133,42 @@ if __name__ == '__main__':
     np.save('{}/{}'.format(dir_name, FILE_STAR), Patch.datas)
     np.save("{}/{}".format(dir_name, FILE_SIG_GAUSSIAN), KDEPatch.sig_gaussian)
     np.save("{}/{}".format(dir_name, FILE_SIG_POISSON), KDEPatch.sig_poisson)
-    np.save("{}/{}".format(dir_name, FILE_MESH), np.array([KDEPatch.x_mesh,
-                                                           KDEPatch.y_mesh]))
+    _meshs = np.array([KDEPatch.x_mesh, KDEPatch.y_mesh])
+    np.save("{}/{}".format(dir_name, FILE_MESH), _meshs)
     print('Done =) \n')
-
-
-    print_sep_line()
-
-
-    if IS_PM_ERROR_CUT:
-        _str1 = 'Creating a Dwarf object within'
-        _str2 = 'for proper motion selection: \n'
-        print('{} rh = %0.4f deg {}'.format(_str1, _str2) % R_HALFLIGHT)
-        # hard coding of the size for pm selection.
-        Dwarf = PatchMWSatellite(NAME, RA_DWARF, DEC_DWARF,
-                                 DISTANCE, R_HALFLIGHT, DATABASE, CATALOG_STR)
-        print(Dwarf.__str__())
-
-        pmra_mean, pmra_std, pmdec_mean, pmdec_std = calc_pm_dwarf_gaia(Dwarf)
-
-        del Dwarf    # free the memory though it might not be necessary
-        print('Removed the Dwarf object since it is no longer needed. \n')
-
-
-        print_sep_line()
-
-
-        print('Selecting proper motion within %d sigma: \n' % N_ERRORBAR)
-        pmramax = pmra_mean + N_ERRORBAR * pmra_std
-        pmramin = pmra_mean - N_ERRORBAR * pmra_std
-        pmdecmax = pmdec_mean + N_ERRORBAR * pmdec_std
-        pmdecmin = pmdec_mean - N_ERRORBAR * pmdec_std
-
-        # proper motion selection
-        Patch.mask_cut("pmra", pmramin, pmramax)
-        Patch.mask_cut("pmdec", pmdecmin, pmdecmax)
-
-        print('Creating a KDEPatch object and start the KDE calcuation: \n')
-        KDEPatch = KDE_MWSatellite(RA, DEC, WIDTH, PIXEL_SIZE, SIGMA1, SIGMA2, SIGMA3)
-        print(KDEPatch.__str__())
-
-        execute_kde_routine(Patch, KDEPatch)
-
-        print('Saving datas, sigs, meshgrids ...')
-        np.save('{}/{}-pm{}std'.format(dir_name, FILE_STAR, N_ERRORBAR), Patch.datas)
-        np.save("{}/{}-pm{}std".format(dir_name,
-                FILE_SIG_GAUSSIAN, N_ERRORBAR), KDEPatch.sig_gaussian)
-        np.save("{}/{}-pm{}std".format(dir_name,
-                FILE_SIG_POISSON, N_ERRORBAR), KDEPatch.sig_poisson)
-        np.save("{}/{}-pm{}std".format(dir_name, FILE_MESH,
-                N_ERRORBAR), np.array([KDEPatch.x_mesh, KDEPatch.y_mesh]))
-        print('Done =) \n')
-
     print('Finished KDE calculation. \n')
-
-
     print_sep_line()
-
-
     print('Generating plots and tables ... \n')
-
-
-    # TODO OVERLAPPING FILE NAME TO BE FIXED!!!
-
 
     # visualize searching results
     fig_name = dir_name.replace("results/", "")
+
     plot_dir = "plots"
-    create_dir("plots")
-
-    visual_dir = "{}/visual".format(plot_dir)
-    create_dir(visual_dir)
-    # visualize_4_panel(dir_name, "{}/{}".format(visual_dir, fig_name),
-    #                   N_ERRORBAR, "gaussian")
-    if DATABASE == 'gaia_dr2.gaia_source':
-        visualize_4_panel(dir_name, "{}/{}".format(visual_dir, fig_name), N_ERRORBAR, "poisson")
-    else:
-        visualize_2_panel(dir_name, "{}/{}".format(visual_dir, fig_name), "poisson")
-
-
-    hist_dir = "{}/hist".format(plot_dir)
-    create_dir(hist_dir)
-    # hist_2_panel(dir_name, "{}/{}".format(hist_dir, fig_name),
-    #              N_ERRORBAR, "gaussian")
-    if DATABASE == 'gaia_dr2.gaia_source':
-        hist_4_panel(dir_name, "{}/{}".format(hist_dir, fig_name), N_ERRORBAR, "poisson")
-    else:
-        hist_2_panel(dir_name, "{}/{}".format(hist_dir, fig_name), "poisson")
-
     peaks_dir = "peaks"
+    create_dir("plots")
     create_dir(peaks_dir)
 
-    star_dir = "{}/stars".format(peaks_dir)
-    create_dir(star_dir)
-    # summarize_peaks_star_csv(dir_name, "{}/{}".format(star_dir, fig_name),
-    #                          N_ERRORBAR, "gaussian")
-    if DATABASE == 'gaia_dr2.gaia_source':
-        summarize_peaks_star_csv_gaia(dir_name,
-            "{}/{}".format(star_dir, fig_name), N_ERRORBAR, "poisson")
-    else:
-        summarize_peaks_star_csv(dir_name,
-            "{}/{}".format(star_dir, fig_name), "poisson")
+    visual_dir = "{}/visual".format(plot_dir)
+    hist_dir = "{}/hist".format(plot_dir)
+    create_dir(visual_dir)
+    create_dir(hist_dir)
 
+    star_dir = "{}/stars".format(peaks_dir)
     pixel_dir = "{}/pixels".format(peaks_dir)
+    create_dir(star_dir)
     create_dir(pixel_dir)
-    # summarize_peaks_pixel_csv(dir_name, "{}/{}".format(pixel_dir, fig_name),
-    #                           N_ERRORBAR, "gaussian", RA, DEC, WIDTH)
-    if DATABASE == 'gaia_dr2.gaia_source':
-        summarize_peaks_pixel_csv_gaia(dir_name, "{}/{}".format(pixel_dir, fig_name),
-                              N_ERRORBAR, "poisson", RA, DEC, WIDTH)
-    else:
-        summarize_peaks_pixel_csv(dir_name, "{}/{}".format(pixel_dir, fig_name),
-                              "poisson", RA, DEC, WIDTH)
+
+    _kernels = ['gaussian', 'poisson']
+
+    for k in _kernels:
+        visualize_2_panel(dir_name, "{}/{}".format(visual_dir, fig_name), k)
+        hist_2_panel(dir_name, "{}/{}".format(hist_dir, fig_name), k)
+
+    _name_star = "{}/{}".format(star_dir, fig_name)
+    _name_pixel = "{}/{}".format(pixel_dir, fig_name)
+    summarize_peaks_star_csv(dir_name, _name_star, 'poisson')
+    summarize_peaks_pixel_csv(dir_name, _name_pixel, 'poisson', RA, DEC, WIDTH)
+
 
     print("Done. \n")
     print("We are finished :) \n")
